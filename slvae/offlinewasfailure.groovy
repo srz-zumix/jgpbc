0216fbb46581ffb1e249b694c58998ba4523f09c
srz_zumix <zumix.cpp@gmail.com>
2014-06-20 22:33:18 +0900

    add to offline groovy

---
+++ slvae/offlinewasfailure.groovy
@@ -0,0 +1,25 @@
+import hudson.slaves.OfflineCause.SimpleOfflineCause
+
+class OfflineMessage extends org.jvnet.localizer.Localizable {
+  String message
+  OfflineMessage(String msg) {
+    super(null, null, [])
+    this.message = msg
+  }
+  String toString() {
+    this.message
+  }
+  String toString(java.util.Locale l) {
+    toString()
+  }
+}
+
+if( manager.build.getResult().isWorseThan(hudson.model.Result.SUCCESS) ) {
+    def timestr = new Date().format("HH:mm dd/MM/yy z", TimeZone.getTimeZone("UTC"))
+    def msg = "automated offline: ${manager.build.getProject().getDisplayName()}#${manager.build.number} : " + timestr
+    def cause = SimpleOfflineCause.create(new OfflineMessage(msg))
+    def com = manager.build.getBuiltOn().toComputer()
+    if( com.isOnline() ) {
+        com.setTemporarilyOffline(true, cause)
+    }
+}
