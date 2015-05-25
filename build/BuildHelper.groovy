2a21439efe248ee7c73c61cf10e269beb2770653
srz_zumix <zumix.cpp@gmail.com>
2015-05-25 18:24:39 +0900

    upstream test

--- build/BuildHelper.groovy
+++ build/BuildHelper.groovy
@@ -3,6 +3,13 @@ static keepLog(manager)
 {
   if( manager.build.canToggleLogKeep() ) {
     manager.build.keepLog()
+  } else {
+    def upstreamBuild = manager.build.getUpstreamRelationshipBuild()
+    if( upstreamBuild.getNaem() == manager.build.getName() ) {
+      if( upstreamBuild.canToggleLogKeep() ) {
+        upstreamBuild.keepLog()
+      }
+    }
   }
 }
 
